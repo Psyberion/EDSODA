f781168c9e1c66bb65b5e3114f34e593b1784dd1
David McMurray <david.mcmurray@psyberion.com>
2021-07-29 07:31:29 +0100

    Added NoSQL version

---
+++ EDSODA_NoSQL/Main.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+/*
+    EDSODA - Elite Dangerous Ships Onboard Data Acquisition
+    Copyright (C) 2021  David McMurray
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+namespace EDSODA
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnControl = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnControl
+            // 
+            this.btnControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnControl.Location = new System.Drawing.Point(11, 10);
+            this.btnControl.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnControl.Name = "btnControl";
+            this.btnControl.Size = new System.Drawing.Size(407, 51);
+            this.btnControl.TabIndex = 0;
+            this.btnControl.Text = "Start";
+            this.btnControl.UseVisualStyleBackColor = true;
+            this.btnControl.Click += new System.EventHandler(this.btnControl_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 76);
+            this.Controls.Add(this.btnControl);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Main";
+            this.Text = "EDSODA NoSQL v1.00a by David McMurray";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnControl;
+    }
+}
+
